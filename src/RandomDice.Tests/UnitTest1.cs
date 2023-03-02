f90eaffc30d8a7cf0f4cb00dc0a3b520b16a8721
Luo Lei <j-luolei@msn.com>
2023-03-02 23:10:52 +0800

    add the test method

--- src/RandomDice.Tests/UnitTest1.cs
+++ src/RandomDice.Tests/UnitTest1.cs
@@ -1,10 +1,14 @@
 namespace RandomDice.Tests;
 
+
 public class UnitTest1
 {
     [Fact]
     public void Test1()
     {
-
+        Dice dice  = new Dice();
+        int result = dice.Roll();
+        //Assert.IsTrue(result);
+        //Assert.IsTrue(result <= 6, "The result was greater than six");
     }
 }
